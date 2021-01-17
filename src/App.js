e5508cb8e2ce86e3c74b628faa703ad143ffd5b9
Hakan Sander <hakansander@gmail.com>
2021-01-18 00:02:25 +0300

    Init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {CurrencyList} from "./components/CurrencyList";
+
+function App() {
+  return (
+    <CurrencyList/>
+  );
+}
+
+export default App;
