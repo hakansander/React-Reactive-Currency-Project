2ee86ff3142d661ec78434134abd6c31c850eddf
Hakan Sander <hakansander@gmail.com>
2021-01-28 00:16:36 +0300

    Add real time currency event listening

---
+++ src/components/Currency.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import '../styles.css';
+
+export const Currency = ({currency}) => {
+    const [ccolor, setCcolor] = useState('yellow');
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        //handleToggle(e.currentTarget.id)
+        setCcolor('blue')
+    }
+
+    return (
+        <tr id={currency.id} key={currency.base + currency.target} name="currency" value={currency.id} onClick={handleClick} className={currency.base ? "todo strike" : "todo"}>
+            <td>{currency.base}</td>
+            <td>${currency.target}</td>
+            <td style={{ backgroundColor: ccolor }}>{currency.rate}</td>
+        </tr>
+    );
+};
