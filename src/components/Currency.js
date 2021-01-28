c859a61f95c124e37af3edb79a4bf03ce52ace03
Hakan Sander <hakansander@gmail.com>
2021-01-29 00:36:15 +0300

    Add color change on exchange rate differences

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,14 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import '../styles.css';
 
 export const Currency = ({currency}) => {
     const [ccolor, setCcolor] = useState('yellow');
+
+    const prevRate = useRef();
 
     const handleClick = (e) => {
         e.preventDefault()
         //handleToggle(e.currentTarget.id)
         setCcolor('blue')
     }
+
+    useEffect(() => {
+        if (currency.rate !== prevRate.current) {
+            if(currency.rate > prevRate.current) {
+                setCcolor('green');
+            }
+
+            else if(currency.rate < prevRate.current) {
+                setCcolor('red');
+            }
+
+            prevRate.current = currency.rate;
+        }
+    }, [currency.rate]);
 
     return (
         <tr id={currency.id} name="currency" value={currency.id} onClick={handleClick} className={currency.base ? "todo strike" : "todo"}>
