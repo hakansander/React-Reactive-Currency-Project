e6e9115fba183820a583b68f9b5ec8ef461570e6
Hakan Sander <hakansander@gmail.com>
2021-02-03 23:11:06 +0300

    Add datetime picker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import {RealTimeCurrencyTable} from "./components/RealTimeCurrencyTable";
 import {HistoricalCurrencyTable} from "./components/HistoricalCurrencyTable";
+import {CurrencyDatePicker} from "./components/CurrencyDatePicker";
 import React from "react";
+import {HistoricalCurrencyForm} from "./components/HistoricalCurrencyForm";
 
 function App() {
   return (
--- src/components/HistoricalCurrencyForm.js
+++ src/components/HistoricalCurrencyForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import ReactDatePicker from "react-datepicker";
+import {CurrencyDatePicker} from "./CurrencyDatePicker";
+import {formatDate} from "../utils/DateFormatter";
 
 export const HistoricalCurrencyForm = ({ addCurrency }) => {
 
     const [ baseInput, setBaseInput ] = useState('');
     const [ targetInput, setTargetInput ] = useState('');
     const [ rateInput, setRateInput ] = useState('');
+    const [ currencyDate, setCurrencyDate] = useState('');
 
     const handleBaseChange = (e) => {
         setBaseInput(e.currentTarget.value)
@@ -18,19 +22,24 @@ export const HistoricalCurrencyForm = ({ addCurrency }) => {
         setRateInput(e.currentTarget.value)
     }
 
+    const handleCurrencyDateChange = (currentDate) => {
+        let formattedDate = formatDate(currentDate);
+        setCurrencyDate(formattedDate);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         addCurrency({base: baseInput, target: targetInput, rate: rateInput});
         setBaseInput("");
         setTargetInput("");
         setRateInput("");
-
     }
     return (
         <form onSubmit={handleSubmit}>
             <input value={baseInput} type="text" onChange={handleBaseChange} placeholder="Enter base currency..."/>
             <input value={targetInput} type="text" onChange={handleTargetChange} placeholder="Enter target currency..."/>
             <input value={rateInput} type="text" onChange={handleRateChange} placeholder="Enter exchange rate..."/>
+            <CurrencyDatePicker value={currencyDate} onCurrencyDateSelect={handleCurrencyDateChange} />
             <button>Submit</button>
         </form>
     );
