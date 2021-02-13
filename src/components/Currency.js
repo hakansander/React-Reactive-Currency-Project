f190b9a574cd10a35304841e593853727f749885
Hakan Sander <hakansander@gmail.com>
2021-02-14 00:41:33 +0300

    Add cost and benefit for currency purchase history

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,7 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react';
 import '../styles.css';
 
-export const Currency = ({ currency, removeCurrency, index }) => {
+export const Currency = ({ currency, removeCurrency, index, currentExchangeValues }) => {
     const [ccolor, setCcolor] = useState('yellow');
 
     const prevRate = useRef();
@@ -28,16 +28,17 @@ export const Currency = ({ currency, removeCurrency, index }) => {
 
     return (
         <tr id={currency.id} key={index} name="currency" value={currency.id} onClick={handleClick} className={currency.base ? "todo strike" : "todo"}>
-
             <td><button style={{ background: "red" }} onClick={() => removeCurrency(index)}>x</button></td>
 
             <td>{currency.base}</td>
             <td>{currency.target}</td>
             <td>{currency.date}</td>
-            <td style={{ backgroundColor: ccolor }}>{(parseFloat(currency.rate)).toFixed(2)}</td>
-            <td style={{ backgroundColor: ccolor }}>{(parseFloat(currency.amount)).toFixed(2)}</td>
-            <td style={{ backgroundColor: ccolor }}>{(parseFloat(currency.rate) * parseFloat(currency.amount)).toFixed(2)}</td>
-
+            <td style={{ backgroundColor: ccolor }}> {(parseFloat(currency.rate)).toFixed(2)} </td>
+            <td style={{ backgroundColor: ccolor }}> {(parseFloat(currency.amount)).toFixed(2)} </td>
+            <td style={{ backgroundColor: ccolor }}> {(parseFloat(currency.rate) * parseFloat(currency.amount)).toFixed(2)} </td>
+            <td> {((1 / parseFloat(currentExchangeValues.rates[currency.base])) * parseFloat(currency.amount)).toFixed(2)} </td>
+            <td> {(((1 / parseFloat(currentExchangeValues.rates[currency.base])) * parseFloat(currency.amount)) - (parseFloat(currency.rate) * parseFloat(currency.amount))).toFixed(2)} </td>
+            <td> {((100 * (((1 / parseFloat(currentExchangeValues.rates[currency.base])) * parseFloat(currency.amount)) - (parseFloat(currency.rate) * parseFloat(currency.amount)))) / (parseFloat(currency.rate) * parseFloat(currency.amount))).toFixed(2)}% </td>
         </tr>
     );
 };
--- src/components/HistoricalCurrencyTable.js
+++ src/components/HistoricalCurrencyTable.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Currency} from "./Currency";
 import {HistoricalCurrencyForm} from "./HistoricalCurrencyForm";
+import axios from "axios";
+import {formatDate} from "../utils/DateFormatter";
 
 export const HistoricalCurrencyTable = () => {
     const [currencyList, setCurrencyList] = useState('');
+    const [currentExchangeValues, setCurrentExchangeValues] = useState(null);
+    const [ baseInput, setBaseInput ] = useState('TRY');
+    const [ targetInput, setTargetInput ] = useState('EUR,USD');
+    const [ currencyDate, setCurrencyDate] = useState(formatDate(new Date()));
 
     const addCurrency = inputCurrency => {
         setCurrencyList(prevCurrencies => [
@@ -16,6 +22,43 @@ export const HistoricalCurrencyTable = () => {
         newCurrencyList.splice(index, 1);
         setCurrencyList(newCurrencyList);
     };
+
+    useEffect( () => {
+        axios.get(`https://currency-app-spring-boot.herokuapp.com/currency/${currencyDate}/${baseInput}?targetCurrencies=${targetInput}`)
+            .then(response => {
+                let responseData = response.data.data;
+
+                setCurrentExchangeValues(
+                    {
+                        base: responseData.base,
+                        rates: responseData.rates,
+                        date: currencyDate
+                    }
+                );
+
+                console.log(currentExchangeValues);
+
+            }).catch(error => console.log(error));
+    }, [])
+
+
+    const getCurrentExchangeValues = () => {
+            axios.get(`https://currency-app-spring-boot.herokuapp.com/currency/${currencyDate}/${baseInput}?targetCurrencies=${targetInput}`)
+                .then(response => {
+                    let responseData = response.data.data;
+                    const rateTargetKey = Object.keys(responseData.rates).toString();
+/*                    setCurrentExchangeValues(
+                        {
+                            base: responseData.base,
+                            target: rateTargetKey,
+                            rate: responseData.rates[rateTargetKey],
+                            amount: rateInput,
+                            date: currencyDate
+                        }
+                    )*/
+                })
+                .catch(error => console.log(error));
+    }
 
     return (
         <div>
@@ -45,12 +88,21 @@ export const HistoricalCurrencyTable = () => {
                         <th>
                             <button type="button">Total Amount</button>
                         </th>
+                        <th>
+                            <button type="button">Current Amount</button>
+                        </th>
+                        <th>
+                            <button type="button">Cost/Benefit</button>
+                        </th>
+                        <th>
+                            <button type="button">Percentage Change</button>
+                        </th>
                     </tr>
                 </thead>
 
                 <tbody>
                     {currencyList !== '' && currencyList.map((currency, index) => (
-                        <Currency index={index} key={index} currency={currency} removeCurrency={removeCurrency}/>
+                        <Currency index={index} key={index} currency={currency} removeCurrency={removeCurrency} currentExchangeValues={currentExchangeValues}/>
                     ))}
                 </tbody>
             </table>
